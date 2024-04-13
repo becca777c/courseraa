e8e1f2e5bdbf984bd869922781fb9d74c32bd872
Becca C <149291119+becca777c@users.noreply.github.com>
2024-04-13 10:28:21 +0800

    a

---
+++ recipes.js
@@ -0,0 +1,31 @@
+const recipes = [
+    { 
+      id: 1,
+      title: "Greek salad",
+      price: 12.99,
+      image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae dolorum laudantium exercitationem iure modi molestias fuga?",
+    },
+    {
+      id: 2,
+      title: "Bruchetta",
+      price: 5.99,
+      image: "https://i.ibb.co/317GqZv/Bruchetta.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae dolorum laudantium exercitationem iure modi",
+    },
+    {
+      id: 3,
+      title: "Lemon Dessert",
+      price: 4.78,
+      image: "https://i.ibb.co/sm6MfcH/desert.jpg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae dolorum laudantium exercitationem",
+    },
+  
+  ];
+  
+  export default recipes;
+  
+  
